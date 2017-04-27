aed07abde107e63dadbd1e6446ad716055116514
mylinh123 <mylinhqn2002@gmail.com>
2017-04-27 20:53:39 +0700

    vi du 8 bo dem khong su dung ActionListener

---
+++ btap/vd8_AWTCounterAnonymousInnerClass.java
@@ -0,0 +1,39 @@
+﻿import java.awt.*;
+import java.awt.event.*;
+ 
+public class AWTCounterAnonymousInnerClass extends Frame {
+	
+   private TextField tfCount;
+   private Button btnCount;
+   private int count = 0;
+ 
+   public AWTCounterAnonymousInnerClass () {
+      setLayout(new FlowLayout()); 
+      add(new Label("Counter"));    
+      tfCount = new TextField("0", 10);
+      tfCount.setEditable(false);   
+      add(tfCount);                  
+ 
+      btnCount = new Button("Count");
+      add(btnCount);                
+ 
+      // Construct an anonymous instance of an anonymous class.
+      // btnCount adds this instance as a ActionListener.
+      btnCount.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent evt) {
+            ++count;
+            tfCount.setText(count + "");
+         }
+      });
+ 
+      setTitle("Bộ đếm - AWT Counter");
+      setSize(250, 100);
+      setVisible(true);
+   }
+ 
+   // The entry main method
+   public static void main(String[] args) {
+      new AWTCounterAnonymousInnerClass(); // Let the constructor do the job
+   }
+}
